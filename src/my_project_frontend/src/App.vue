cfdde80c15d5e1bb3b555540e65b16647d93d669
Tutel 3 <mikolajzolna16@gmail.com>
2024-11-29 10:23:09 +0000

    added msg

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,28 +1,30 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let displasyMsg = ref('');
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+  const msg = target.querySelector('#msg').value;
+  await my_project_backend.save_msg(msg)
+  await getMsg()
+}
+async function getMsg() {
+  displasyMsg.value =await my_project_backend.get_msg()
 }
 </script>
-
+getMsg()
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
+      <label for="msg">Enter your msg: &nbsp;</label>
+      <input id="msg" alt="msg" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="displasyMsg">{{ displasyMsg }}</section>
   </main>
 </template>
